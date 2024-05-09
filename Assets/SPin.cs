6384f333ce5ab23b834b05a094082f7e7ed97b15
김성진 <helloksj12@gmail.com>
2024-05-09 17:38:44 +0900

    몬스터 , 주사위 배치,  UI 해상도별 대응 조정

---
+++ Assets/SPin.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SPin : MonoBehaviour
+{
+    public int repeat_i;
+    private float spinsecond = 0.03f;
+    public float stopsecond;
+    public float x;
+    public float y;
+    public float z;
+    Coroutine a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = StartCoroutine(spindice());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StopCoroutine(a);
+            Debug.Log("d");
+            a =StartCoroutine(spindice());
+            
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("d");
+        }
+    }
+
+        
+    IEnumerator spindice()
+    {
+        int i;
+        for (i = 0; i <= repeat_i; i++)
+        {
+            transform.Rotate(new Vector3(50, 0, 50));
+            yield return new WaitForSeconds(spinsecond);
+        }
+        transform.rotation = Quaternion.Euler(x, y, z);
+    }
+        
+}
